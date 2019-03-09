d835aa8217aed19b335a70825c9bc85346e1640c
Vlad Rindevich <rindevich.vs@gmail.com>
2019-03-10 00:19:19 +0300

    fix(app): simplify using router Link class

--- src/app/Links.js
+++ src/app/Links.js
@@ -1,9 +1,13 @@
 import {internal, render} from '@corpuscule/element';
-import {api, outlet} from '@corpuscule/router';
+import {api, Link, outlet} from '@corpuscule/router';
 import styles from '@corpuscule/styles';
 import {element, html} from './config';
 import linksStyles from './links.css';
 import {routes} from './routes';
+
+const activate = url => {
+  return location.pathname === url ? 'active' : '';
+};
 
 @element('ex-links')
 @outlet(routes)
@@ -16,20 +20,16 @@ export default class Links extends HTMLElement {
 
   @api @internal layout;
 
-  getActiveClass(url) {
-    return location.pathname === url ? 'active' : '';
-  }
-
   [render]() {
     const {urls} = this.constructor;
 
     return html`
       <ul>
-        <li class="${this.getActiveClass(urls.simple)}">
-          <a is="corpuscule-link" href="${urls.simple}">Simple Form</a>
+        <li class="${activate(urls.simple)}">
+          <a is="${Link}" href="${urls.simple}">Simple Form</a>
         </li>
-        <li class="${this.getActiveClass(urls.recordValidate)}">
-          <a is="corpuscule-link" href="${urls.recordValidate}">Record-Level Validation Form</a>
+        <li class="${activate(urls.recordValidate)}">
+          <a is="${Link}" href="${urls.recordValidate}">Record-Level Validation Form</a>
         </li>
       </ul>
     `;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// Allows to use any Custom Element class definition as a placeholder for its
+// name
+import '@corpuscule/lit-html-renderer/lib/withCustomElement';
 import './app';
